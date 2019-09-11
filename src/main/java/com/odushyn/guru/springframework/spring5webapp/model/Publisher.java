e71a97e1ffb58b865f6d6ec0cb8a1f771ac298e2
e211320 <oleksander.dushyn@evry.com>
2019-09-11 23:04:05 +0200

    add books controller

--- src/main/java/com/odushyn/guru/springframework/spring5webapp/model/Publisher.java
+++ src/main/java/com/odushyn/guru/springframework/spring5webapp/model/Publisher.java
@@ -16,6 +16,9 @@ public class Publisher {
     public String name;
     public String address;
 
+    public Publisher() {
+    }
+
     public Publisher(String name, String address) {
         this.name = name;
         this.address = address;
